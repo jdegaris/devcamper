75d606cc60458338d7637b904ce1b56b1304dca1
jdegaris <j.a.degaris@gmail.com>
2020-02-01 02:12:38 -0500

    setup MongoDB - created Bootcamp Model

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({
-    path: './config/config.env'
-})
+  path: './config/config.env'
+});
+
+// Connect to Database
+connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
